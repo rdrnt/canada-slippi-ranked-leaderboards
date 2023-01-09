454ee40b935199251754fb238ca376e08a556ca2
riley drnt <rileyduraant@gmail.com>
2023-01-09 10:42:48 -0500

    [CHORE]: Chane project name

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Base application title used in document.title
-  title: 'Ontario Slippi Ranked Leaderboard',
+  title: 'Canada Slippi Ranked Leaderboard',
 
   // use cname option to add CNAME file to webpack build
   // CNAME file allows to use custom domain names with gh-pages, example:
@@ -9,7 +9,7 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'https://rdrnt.github.io/ontario-melee-leaderboards/',
+  repoPath: 'https://rdrnt.github.io/canada-slippi-ranked-leaderboards/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
